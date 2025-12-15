9d26d5b488c53584760906b09b694e8b4ebdb47d
Fritz <fritz@junoservices.com>
2025-12-16 09:52:32 +1300

    Removed NPOI Spreadsheet helper class and all references to NPOI. Re-ran all unit tests OK

--- obj/Debug/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
+++ obj/Debug/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a1b901e008c93f52c5a9dfeb08e80d20661d1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9267f6dfce7b3d4486fdb53394221b0a0e3b157")]
 [assembly: System.Reflection.AssemblyProductAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
+++ obj/Release/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7292e51422a537bb90923c6e57ee03940ff903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9267f6dfce7b3d4486fdb53394221b0a0e3b157")]
 [assembly: System.Reflection.AssemblyProductAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
