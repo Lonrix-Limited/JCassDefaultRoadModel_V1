61ac1490f4b5d66cbbe6a668bd75001bc05f9b33
Fritz <fritz@junoservices.com>
2025-12-18 11:20:43 +1300

    Monte Carlo refinements.

--- obj/Debug/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
+++ obj/Debug/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e05844913b64a2290ee22efed1b2527c266c756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a8b676ba6379021d1782730dbc48862f22b9c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
