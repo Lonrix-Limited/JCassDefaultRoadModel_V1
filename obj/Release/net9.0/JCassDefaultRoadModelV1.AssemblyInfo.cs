47787441b7ed99bd92b4940e7175834b207848a8
Fritz <fritz@junoservices.com>
2026-03-11 13:10:37 +1300

    New Build

--- obj/Release/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
+++ obj/Release/net9.0/JCassDefaultRoadModelV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b3f55f5e621a3cddbcfc19d25623b18c5ef921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8747aa4491dbd1ace319c3b0e99317ea65f0ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JCassDefaultRoadModelV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
